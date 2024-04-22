603c4d833f41584912b34e732c8a875740ee85af
Dar1usss <118019309+danissimoae@users.noreply.github.com>
2024-04-23 03:15:00 +0500

    Update OOP_Laba_3.cpp

--- OOP_Laba_3.cpp
+++ OOP_Laba_3.cpp
@@ -8,9 +8,26 @@ using namespace std;
 template <class T>
 class CustomArray {
 private:
-	T *values;
+	T* values;
 	int size;
 public:
+	CustomArray(int size) {
+		this->size = size;
+		this->values = new T[size];
+	}
+
+	CustomArray(CustomArray& a) {
+		this->size = a.getSize();
+		this->values = new T[this->size];
+		for (int i = 0; i < this->size; i++) {
+			values[i] = a.getValue(i);
+		}
+	}
+
+	~CustomArray() {
+		delete[] values;
+	}
+
 	int getSize() {
 		return size;
 	}
@@ -46,6 +63,9 @@ public:
 	}
 
 	void addIn(T elem, int idx) {
+		if (idx < 0 || idx > size) {
+			throw out_of_range("Index out of range");
+		}
 		size = size + 1;
 		T* newValues = new T[size];
 		for (int i = 0; i < idx; i++) {
@@ -53,42 +73,28 @@ public:
 		}
 		newValues[idx] = elem;
 		for (int i = idx + 1; i < size; i++) {
-			newValues[i] = values[i];
+			newValues[i] = values[i - 1];
 		}
 		delete[] values;
 		values = newValues;
 	}
 
 	T pop(int idx) {
+		if (idx < 0 || idx >= size) {
+			throw out_of_range("Index out of range");
+		}
+		T elem = values[idx];
 		size = size - 1;
 		T* newValues = new T[size];
 		for (int i = 0; i < idx; i++) {
 			newValues[i] = values[i];
 		}
-		T elem = values[idx];
-		for (int i = idx; i < size; i++) {
-			newValues[i] = values[i];
+		for (int i = idx + 1; i < size + 1; i++) {
+			newValues[i - 1] = values[i];
 		}
 		delete[] values;
 		values = newValues;
 		return elem;
-	}
-
-	CustomArray(int size) {
-		this->size = size;
-		this->values = new T[size];
-	}
-
-	CustomArray(CustomArray& a) {
-		this->size = a.getSize();
-		this->values = new T[this->size];
-		for (int i = 0; i < this->size; i++) {
-			values[i] = a.getValue(i);
-		}
-	}
-
-	~CustomArray() {
-		delete[] values;
 	}
 };
 
@@ -122,99 +128,42 @@ public:
 };
 
 
+void performRandomActions(CustomArray<Trees*>& forest, int numActions) {
+	for (int i = 0; i < numActions; i++) {
+		int randomAction = rand() % 3;
+		if (randomAction == 0) { // Add
+			int randomIndex = rand() % (forest.getSize() + 1);
+			forest.addIn(new Oak(), randomIndex);
+		}
+		else if (randomAction == 1 && forest.getSize() > 0) { // Remove
+			int randomIndex = rand() % forest.getSize();
+			Trees* tree = forest.getValue(randomIndex);
+			forest.pop(randomIndex);
+			delete tree;
+		}
+		else { // Method call
+			if (forest.getSize() > 0) {
+				int randomIndex = rand() % forest.getSize();
+				forest.getValue(randomIndex)->leaf();
+			}
+		}
+	}
+}
+
 int main() {
-    CustomArray<Trees*> forest(4);
-    for (int i = 0; i < forest.getSize(); i++) {
-        if (i % 2) {
-            forest.setValue(i, new Pine());
-        }
-        else {
-            forest.setValue(i, new Maple());
-        }
-    }
+	srand(time(nullptr));
 
-    for (int i = 0; i < forest.getSize(); i++) {
-        forest.getValue(i)->leaf();
-    }
+	cout << "100 случайных действий:" << endl;
+	CustomArray<Trees*> forest1(4);
+	performRandomActions(forest1, 100);
 
+	cout << "1000 случайных действий:" << endl;
+	CustomArray<Trees*> forest2(4);
+	performRandomActions(forest2, 1000);
 
-    cout << endl;
+	cout << "10000 случайных действий:" << endl;
+	CustomArray<Trees*> forest3(4);
+	performRandomActions(forest3, 10000);
 
-
-    forest.addFirst(new Oak());
-
-    for (int i = 0; i < forest.getSize(); i++) {
-        forest.getValue(i)->leaf();
-    }
-
-
-    cout << endl;
-
-
-    forest.addLast(new Oak());
-
-    for (int i = 0; i < forest.getSize(); i++) {
-        forest.getValue(i)->leaf();
-    }
-
-
-    cout << endl;
-
-
-    forest.addIn(new Oak(), 3);
-
-    for (int i = 0; i < forest.getSize(); i++) {
-        forest.getValue(i)->leaf();
-    }
-
-
-    cout << endl << endl << endl;
-
-    clock_t start = clock();
-    for (int i = 0; i < 100; i++) {
-        int random_action;
-        if (forest.getSize() == 0) {
-            random_action = 0;
-        }
-        else {
-            random_action = rand();
-        }
-        if (random_action % 3 == 0) {
-            Trees* tree;
-            int random_object = rand();
-            if (random_object % 3 == 0) {
-                tree = new Pine();
-            }
-            else if (random_action % 3 == 1) {
-                tree = new Maple();
-            }
-            else {
-                tree = new Oak();
-            }
-            int random_index;
-            if (forest.getSize() > 0) {
-                random_index = rand() % forest.getSize();
-            }
-            else {
-                random_index = 0;
-            }
-            forest.addIn(tree, random_index);
-        }
-        else if (random_action % 3 == 1) {
-            int random_index = rand() % forest.getSize();
-            Trees* tree = forest.getValue(random_index);
-            forest.pop(random_index);
-            delete tree;
-        }
-        else {
-            int random_index = rand() % forest.getSize();
-            forest.getValue(random_index)->leaf();
-        }
-    }
-
-    clock_t end = clock();
-    double seconds = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("Время: %f секунд \n", seconds);
-
-    return -1;
+	return 0;
 }
