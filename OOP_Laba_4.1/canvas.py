c2e79a466486f23f1514aa0ee93e60805775aca1
Dar1usss <118019309+danissimoae@users.noreply.github.com>
2024-05-09 02:03:45 +0500

    Update canvas.py
    Version 2

--- OOP_Laba_4.1/canvas.py
+++ OOP_Laba_4.1/canvas.py
@@ -15,7 +15,6 @@ class Canvas(QLabel):
 
         self._initUI(x, y, w, h)
 
-
     def _initUI(self, x: int, y: int, w: int, h: int):
         self.paint()
 
@@ -26,13 +25,11 @@ class Canvas(QLabel):
             """
         )
 
-
     def paint(self):
         self._canvasBase.fill(Qt.GlobalColor.white)
         for c in self._container:
             c.draw(self)
         self.setPixmap(self._canvasBase)
-
 
     def deleteSelected(self):
         newContainer = []
@@ -45,35 +42,51 @@ class Canvas(QLabel):
             self._container[-1].select()
         self.paint()
 
-
     def setSelectAll(self, selectAll: bool):
         self._selectAll = selectAll
-
 
     def setSelectFew(self, selectFew: bool):
         self._selectFew = selectFew
 
-
     def getCanvasBase(self):
         return self._canvasBase
-
 
     def mousePressEvent(self, event):
         if not event.button() == Qt.MouseButton.LeftButton:
             return None
 
         position = event.pos()
+        # Создавать ли объект
         flag = True
 
-        if not self._selectAll:
+        if self._selectFew and event.modifiers() & Qt.KeyboardModifier.ControlModifier:
+            for i in range(len(self._container) - 1, -1, -1):
+                circle = self._container[i]
+                if circle.isIncludeDot(position):
+                    circle.select()
+                    break
+            self.paint()
+            return
+
+        for c in self._container:
+            c.unSelect()
+
+        if self._selectAll:
+            # Выделяем только те объекты, которые пересекаются с точкой клика
             for c in self._container:
-                c.unSelect()
+                if c.isIncludeDot(position):
+                    c.select()
+            # Если есть пересекающиеся объекты, завершаем обработку
+            if any(c.isSelected() for c in self._container):
+                self.paint()
+                return
 
         for i in range(len(self._container) - 1, -1, -1):
             circle = self._container[i]
             if circle.isIncludeDot(position):
                 flag = False
                 circle.select()
+                # Если selectAll != True, останавливаемся после первого объекта
                 if not self._selectAll:
                     break
 
@@ -84,11 +97,3 @@ class Canvas(QLabel):
             c.select()
             self._container.append(c)
         self.paint()
-
-
-
-
-
-
-
-
