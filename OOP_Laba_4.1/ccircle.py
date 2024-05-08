6ba66e398a843dc7ef08b9653074afe03f6bc6d7
Dar1usss <118019309+danissimoae@users.noreply.github.com>
2024-05-09 02:04:03 +0500

    Update ccircle.py
    Version 2

--- OOP_Laba_4.1/ccircle.py
+++ OOP_Laba_4.1/ccircle.py
@@ -1,4 +1,4 @@
-from PyQt6.QtCore import QPoint, Qt
+from PyQt6.QtCore import QPoint
 from PyQt6.QtGui import QPainter, QBrush, QColor, QPen
 
 
@@ -9,7 +9,6 @@ class CCircle:
         self._R = 50
         self._isSelected = False
 
-
     def draw(self, canvas):
         painter = QPainter(canvas.getCanvasBase())
         center = QPoint(self._x, self._y)
@@ -17,15 +16,14 @@ class CCircle:
         ry = rx
 
         if self._isSelected:
-            painter.setBrush(QBrush(QColor(255,255,255,0)))
-            painter.setPen(QPen(QColor(120,120,120),1))
+            painter.setBrush(QBrush(QColor(255, 255, 255, 0)))
+            painter.setPen(QPen(QColor(120, 120, 120), 1))
             painter.drawRect(self._x - rx, self._y - ry, rx * 2, ry * 2)
 
-        painter.setBrush(QBrush(QColor(79, 113, 190)))
+        painter.setBrush(QBrush(QColor(52, 237, 148)))
         painter.setPen(QPen(QColor(0, 0, 0), 1))
         painter.drawEllipse(center, rx, ry)
         canvas.setPixmap(canvas.getCanvasBase())
-
 
     def isSelected(self):
         return self._isSelected
