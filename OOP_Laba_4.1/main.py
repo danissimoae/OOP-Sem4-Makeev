a9423953febe30c0345e5c9ba70514aaf2020302
Dar1usss <118019309+danissimoae@users.noreply.github.com>
2024-05-09 02:04:20 +0500

    Update main.py
    Version 2

--- OOP_Laba_4.1/main.py
+++ OOP_Laba_4.1/main.py
@@ -1,4 +1,6 @@
 import sys
+
+from PyQt6.QtGui import QIcon
 from PyQt6.QtWidgets import QApplication
 from mainWindow import MainWindow
 
@@ -6,4 +8,6 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     main_window = MainWindow()
     main_window.show()
+    main_window.setWindowTitle("Круги на форме")
+    main_window.setWindowIcon(QIcon("windowIcon.png"))
     sys.exit(app.exec())
